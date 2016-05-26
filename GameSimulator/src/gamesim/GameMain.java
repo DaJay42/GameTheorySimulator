cbc84c4c0e5b4c70c060694c2edd66f758c20a32
DaJay42 <DaJay42@users.noreply.github.com>
2016-05-27 00:24:27 +0200

    Small cleanup; removed unnecessary public/static modifiers.

--- GameSimulator/src/gamesim/GameMain.java
+++ GameSimulator/src/gamesim/GameMain.java
@@ -39,7 +39,7 @@ import gamesim.gamerules.*;
  * If not specified, System.out will be used instead.
  * 
  * @param -i : Path to a file from which to read more arguments.
- * \r, \n, \t are treated as spaces.
+ * All whitespace (\r, \n, \t and space) are valid separators.
  * All arguments passed after this will be ignored.
  * If specified multiple times (i.e. in the file(s)), the evaluation will be chained.
  * 
@@ -47,106 +47,100 @@ import gamesim.gamerules.*;
  */
 public class GameMain {
 	
-	//defaults
-	public static PrintStream out = System.out;
-	public static Class<? extends GameTourney> tourney = EconomicModel.class;
-	public static String[] tourneyArgs = {};
-	public static Class<? extends GameRuleSet> ruleSet = AxelrodPrisoners.class;
-	public static ArrayList<Class<? extends GameStrategy>> listStrategies;
 	
-	public static Class<?>[] allStrategies = {
-		FiftyFifty.class,
-		AlwaysC.class, 
-		AlwaysD.class,
-		Alternate.class,
-		
-		TitForTat.class,
-		TitForTwoTat.class,
-		Friedman.class,
-		Downing.class,
-		Joss.class,
-		Pavlov.class,
-		
-		DTitForTat.class,
-		CDowning.class,
-		
-		Switch.class,
-		Panic.class,
-		};
-	/**
-	 * blubb
-	**/
+	public static PrintStream out = System.out;
+	
 	public static void main(String[] args){
 		
-		/* Note to self:
-		 * clazz = Class.forName(classname)
-		 * object = clazz.newInstance();
-		 */
-		try{
-		listStrategies = new ArrayList<Class<? extends GameStrategy>>();
+		//defaults
+		Class<? extends GameRuleSet> ruleSet = AxelrodPrisoners.class;
+		Class<? extends GameTourney> tourney = EconomicModel.class;
+		String[] tourneyArgs = {};
 		ArrayList<String> listArgs = new ArrayList<String>();
+		ArrayList<Class<? extends GameStrategy>> listStrategies = new ArrayList<Class<? extends GameStrategy>>();
+		Class<?>[] allStrategies = {
+			FiftyFifty.class,
+			AlwaysC.class, 
+			AlwaysD.class,
+			Alternate.class,
+			
+			TitForTat.class,
+			TitForTwoTat.class,
+			Friedman.class,
+			Downing.class,
+			Joss.class,
+			Pavlov.class,
+			
+			DTitForTat.class,
+			CDowning.class,
+			
+			Switch.class,
+			Panic.class,
+			};
 		
-		for(int i = 0; i < args.length; i++){
-			switch(args[i]){
-			case "-t":
-				tourney = Class.forName(args[i+1]).asSubclass(GameTourney.class);
-				//default args are useless now
-				tourneyArgs = new String[0];
-				i++;
-				break;
-			case "-r":
-				ruleSet = Class.forName(args[i+1]).asSubclass(GameRuleSet.class);
-				i++;
-				break;
-			case "-s":
-				listStrategies.add(Class.forName(args[i+1].replaceAll(",", "")).asSubclass(GameStrategy.class));
-				while(args[i+1].endsWith(",")){
+		try{
+			for(int i = 0; i < args.length; i++){
+				switch(args[i]){
+				case "-t":
+					tourney = Class.forName(args[i+1]).asSubclass(GameTourney.class);
+					//default args are useless now
+					tourneyArgs = new String[0];
 					i++;
+					break;
+				case "-r":
+					ruleSet = Class.forName(args[i+1]).asSubclass(GameRuleSet.class);
+					i++;
+					break;
+				case "-s":
 					listStrategies.add(Class.forName(args[i+1].replaceAll(",", "")).asSubclass(GameStrategy.class));
-				}
-				break;
-			case "-a":
-				listArgs.add(args[i+1].replaceAll(",", ""));
-				while(args[i+1].endsWith(",")){
-					i++;
+					while(args[i+1].endsWith(",")){
+						i++;
+						listStrategies.add(Class.forName(args[i+1].replaceAll(",", "")).asSubclass(GameStrategy.class));
+					}
+					break;
+				case "-a":
 					listArgs.add(args[i+1].replaceAll(",", ""));
-				}
-				break;
-			case "-i":
-				List<String> fi = Files.readAllLines(FileSystems.getDefault().getPath(args[i+1]));
-				ArrayList<String> lArgs = new ArrayList<String>();
-				for(String line : fi){
-					for(String word : line.replaceAll("\t", " ").split(" ")){
-						if(word.length() > 0){
-							lArgs.add(word);
+					while(args[i+1].endsWith(",")){
+						i++;
+						listArgs.add(args[i+1].replaceAll(",", ""));
+					}
+					break;
+				case "-i":
+					List<String> fi = Files.readAllLines(FileSystems.getDefault().getPath(args[i+1]));
+					ArrayList<String> lArgs = new ArrayList<String>();
+					for(String line : fi){
+						for(String word : line.replaceAll("\t", " ").split(" ")){
+							if(word.length() > 0){
+								lArgs.add(word);
+							}
 						}
 					}
+					args = lArgs.toArray(args);
+					i = -1;
+					break;
+				case "-o":
+					out = new PrintStream(args[i+1]);
+					out.println("/**GameTheorySimulator outputting to "+args[i+1]+"**/");
+					i++;
+				default:
+					break;
 				}
-				args = lArgs.toArray(args);
-				i = -1;
-				break;
-			case "-o":
-				out = new PrintStream(args[i+1]);
-				out.println("/**GameTheorySimulator outputting to "+args[i+1]+"**/");
-				i++;
-			default:
-				break;
 			}
-		}
-		
-		if(listStrategies.size() == 0){
-			for(Class<?> c : allStrategies){
-				if(GameStrategy.class.isAssignableFrom(c))
-					listStrategies.add(c.asSubclass(GameStrategy.class));
+			
+			if(listStrategies.size() == 0){
+				for(Class<?> c : allStrategies){
+					if(GameStrategy.class.isAssignableFrom(c))
+						listStrategies.add(c.asSubclass(GameStrategy.class));
+				}
 			}
-		}
-		
-		if(listArgs.size() > 0){
-			tourneyArgs = listArgs.toArray(tourneyArgs);
-		}
-		
-		GameRunner runner = new GameRunner(ruleSet, listStrategies, tourney, tourneyArgs);
-		runner.run();
+			
+			if(listArgs.size() > 0){
+				tourneyArgs = listArgs.toArray(tourneyArgs);
+			}
+			
+			GameRunner runner = new GameRunner(ruleSet, listStrategies, tourney, tourneyArgs);
+			runner.run();
+			
 		}catch(Exception e){
 			e.printStackTrace();
 			if(out != System.out)
